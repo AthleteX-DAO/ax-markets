9f880825ed1455db9a31630f51f84d55148832e9
Kevin Kamto <kamtokevin@gmail.com>
2025-07-06 13:00:21 -0400

    refactor: cleaned up bottom navigation bar

--- src/components/BottomNavigationBar/BottomNavigationBar.tsx
+++ src/components/BottomNavigationBar/BottomNavigationBar.tsx
@@ -1,16 +1,11 @@
 import React from 'react';
-// import './BottomNavigationBar.css';
 
 const BottomNavigationBar: React.FC = () => {
     return (
         <div className="bottom-navigation-bar">
-            <nav>
-                <ul className="nav-links">
-                    <li><a href="/home">Home</a></li>
-                    <li><a href="/about">About</a></li>
-                    <li><a href="/contact">Contact</a></li>
-                </ul>
-            </nav>
+            <div className="copyright">
+                <p>&copy; {new Date().getFullYear()} Ax Markets. All rights reserved.</p>
+            </div>
         </div>
     );
 };
