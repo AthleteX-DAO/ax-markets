42c9bdd6067e1363c5c5a9da145f07a49cb5e397
Kevin Kamto <kamtokevin@gmail.com>
2025-07-06 12:32:52 -0400

    feat: wip deposit button

--- src/components/TopNavigationBar/widgets/DepositButton.tsx
+++ src/components/TopNavigationBar/widgets/DepositButton.tsx
@@ -3,7 +3,7 @@ import { Button } from "@/components/ui/button"
 
 function DepositButton() {
     return (
-        <Button className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded">
+        <Button variant="secondary">
         Deposit
         </Button>
     )
