572456b495bca1d39221ff32e0887c4aa01c7bb9
Kevin Kamto <kamtokevin@gmail.com>
2025-07-06 17:16:20 -0400

    feat: implementing a top navigation bar

--- src/components/TopNavigationBar/TopNavigationBar.tsx
+++ src/components/TopNavigationBar/TopNavigationBar.tsx
@@ -3,14 +3,14 @@ import DepositButton from "./widgets/DepositButton";
 
 const TopNavigationBar = () => {
     return (
-        <nav className="bg-gray-800 p-4">
+        <nav className="w-full">
             <div className="container mx-auto flex justify-between items-center">
-                <div className="text-white text-lg font-bold">My App</div>
-                <a href="/" className="text-gray-300 hover:text-white">Markets</a>
-                <div className="space-x-4">
-                    <DepositButton />
-                    <AccountSheet />
-                </div>
+            <img src="/src/assets/Athletex 11 BIG.png" alt="AthleteX Markets Exchange" className="h-45" />
+            <a href="/" className="text-gray-300 hover:text-white">Markets</a>
+            <div className="space-x-4">
+                <DepositButton />
+                <AccountSheet />
+            </div>
             </div>
         </nav>
     );
