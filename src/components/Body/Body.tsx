a233c51120372c5f1e26d4e9df103dd72f4269d4
Kevin Kamto <kamtokevin@gmail.com>
2025-07-06 17:25:21 -0400

    feat: added simple labeling to body

--- src/components/Body/Body.tsx
+++ src/components/Body/Body.tsx
@@ -1,14 +1,22 @@
+
 import { FeaturedMarkets } from './widgets/FeaturedMarkets';
 import { RelatedMarkets } from './widgets/RelatedMarkets';
 
 const Body = () => {
 
     return (
-        <div className="App">
-            <FeaturedMarkets />
+        <div className="App container mx-auto px-4 py-8">
+            <div className='my-12'>
+                <h2 className='text-3xl font-bold text-gray-800 mb-6'>Featured Markets</h2>
+                <FeaturedMarkets />
+            </div>
 
-            <RelatedMarkets />
-      </div>
+            <div className="my-12">
+                <h2 className="text-3xl font-bold text-gray-800 mb-6">Related Markets</h2>
+                <RelatedMarkets />
+            </div>
+        
+        </div>
     )
 }
 
