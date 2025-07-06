52d4861aaaa0e6bbf9eab6d31edf251b96e70411
Kevin Kamto <kamtokevin@gmail.com>
2025-07-06 09:46:11 -0400

    feat: these components are empty, but will be filled in

---
+++ src/components/TopNavigationBar/widgets/DepositButton.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button"
+
+
+function DepositButton() {
+    return (
+        <Button className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded">
+        Deposit
+        </Button>
+    )
+    }
+
+export default DepositButton;
