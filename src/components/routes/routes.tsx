9628bd8b7741b72ce28825b2294ad2106349152b
Kevin Kamto <kamtokevin@gmail.com>
2025-07-06 09:45:44 -0400

    feat: created basic routes component

---
+++ src/components/routes/routes.tsx
@@ -0,0 +1,12 @@
+import { Routes, Route } from 'react-router-dom';
+import TopNavigationBar from '../TopNavigationBar/TopNavigationBar';
+
+
+const RoutesComponent = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<div>Home Page</div>} />
+      <Route path="*" element={<div>404 Not Found</div>} />
+    </Routes>
+  );
+}
