da53d5e931a5224facdc6ea76935fdfabfcfa094
Kevin Kamto <kamtokevin@gmail.com>
2025-07-06 17:24:58 -0400

    refactor: styled related & featured markets

--- src/components/Body/widgets/FeaturedMarkets.tsx
+++ src/components/Body/widgets/FeaturedMarkets.tsx
@@ -1,31 +1,56 @@
 
-import { Card, CardContent } from "@/components/ui/card"
+import { useEffect, useState } from "react";
 import {
   Carousel,
   CarouselContent,
   CarouselItem,
   CarouselNext,
   CarouselPrevious,
+  type CarouselApi,
 } from "@/components/ui/carousel"
 
+import { cn } from "@/lib/utils";
+import { MarketCard } from "./MarketCard";
+
+
 export function FeaturedMarkets() {
+  const [api, setApi] = useState<CarouselApi>();
+  const [current, setCurrent] = useState(0);
+  const [count, setCount] = useState(0);
+  useEffect(() => {
+    if (!api) {
+      return;
+    }
+    setCount(api.scrollSnapList().length);
+    setCurrent(api.selectedScrollSnap() + 1);
+    api.on("select", () => {
+      setCurrent(api.selectedScrollSnap() + 1);
+    });
+  }, [api]);
   return (
-    <Carousel className="w-full max-w-xs">
+    <div className="mx-auto max-w-2xl">
+    <Carousel setApi={setApi} className="w-full max-w-2xl">
       <CarouselContent>
         {Array.from({ length: 3 }).map((_, index) => (
           <CarouselItem key={index}>
-            <div className="p-1">
-              <Card>
-                <CardContent className="flex aspect-square items-center justify-center p-6">
-                  <span className="text-4xl font-semibold">{index + 1}</span>
-                </CardContent>
-              </Card>
-            </div>
+            <MarketCard />
           </CarouselItem>
         ))}
       </CarouselContent>
       <CarouselPrevious />
       <CarouselNext />
     </Carousel>
+    <div className="mt-4 flex items-center justify-center gap-2">
+      {Array.from({ length: count }).map((_, index) => (
+        <button
+          key={index}
+          onClick={() => api?.scrollTo(index)}
+          className={cn("h-3.5 w-3.5 rounded-full border-2", {
+            "border-primary": current === index + 1,
+          })}
+        />
+      ))}
+    </div>
+  </div>
   )
 }
--- src/components/Body/widgets/RelatedMarkets.tsx
+++ src/components/Body/widgets/RelatedMarkets.tsx
@@ -1,3 +1,6 @@
+"use client";
+import { useEffect, useState } from "react";
+
 import { Card, CardContent } from "@/components/ui/card"
 import {
   Carousel,
@@ -5,31 +8,53 @@ import {
   CarouselItem,
   CarouselNext,
   CarouselPrevious,
+  type CarouselApi,
+
 } from "@/components/ui/carousel"
 
+import { cn } from "@/lib/utils";
+
+
 export function RelatedMarkets() {
+  const [api, setApi] = useState<CarouselApi>();
+  const [current, setCurrent] = useState(0);
+  useEffect(() => {
+    if (!api) {
+      return;
+    }
+    setCurrent(api.selectedScrollSnap() + 1);
+    api.on("select", () => {
+      setCurrent(api.selectedScrollSnap() + 1);
+    });
+  }, [api]);
   return (
-    <Carousel
-      opts={{
-        align: "start",
-      }}
-      className="w-full max-w-sm"
-    >
-      <CarouselContent>
-        {Array.from({ length: 9 }).map((_, index) => (
-          <CarouselItem key={index} className="md:basis-1/2 lg:basis-1/3">
-            <div className="p-1">
-              <Card>
-                <CardContent className="flex aspect-square items-center justify-center p-6">
-                  <span className="text-3xl font-semibold">{index + 1}</span>
+    <div className="mx-auto max-w-xs">
+      <Carousel
+        setApi={setApi}
+        className="w-full max-w-xs mx-2"
+        opts={{ loop: true }}
+      >
+        <CarouselContent>
+          {Array.from({ length: 5 }).map((_, index) => (
+            <CarouselItem key={index} className="basis-3/5">
+              <Card
+                className={cn(
+                  "bg-primary text-primary-foreground transition-all duration-500",
+                  {
+                    "opacity-30": index !== current - 1,
+                  }
+                )}
+              >
+                <CardContent className="flex aspect-video items-center justify-center p-6">
+                  <span className="text-4xl font-semibold">{index + 1}</span>
                 </CardContent>
               </Card>
-            </div>
-          </CarouselItem>
-        ))}
-      </CarouselContent>
-      <CarouselPrevious />
-      <CarouselNext />
-    </Carousel>
-  )
+            </CarouselItem>
+          ))}
+        </CarouselContent>
+        <CarouselPrevious />
+        <CarouselNext />
+      </Carousel>
+    </div>
+  );
 }
