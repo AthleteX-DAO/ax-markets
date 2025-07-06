c61920539afd865f4e2528522addfb9adc9f2674
Kevin Kamto <kamtokevin@gmail.com>
2025-07-06 09:29:25 -0400

    refactor: upgraded app with basic theme + nav bars

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,32 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { ThemeProvider } from "@/components/theme-provider"
+
 import './App.css'
+import TopNavigationBar from './components/TopNavigationBar/TopNavigationBar'
+import BottomNavigationBar from './components/BottomNavigationBar/BottomNavigationBar'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <ThemeProvider defaultTheme="dark" storageKey="ax-ui-theme">
+      <TopNavigationBar />
+
+      <div className="App">
+        <header className="App-header">
+          <h1>Welcome to AX Markets</h1>
+          <p>
+            Edit <code>src/App.tsx</code> and save to test HMR.
+          </p>
+          <button onClick={() => setCount((count) => count + 1)}>
+            Count is: {count}
+          </button>
+        </header>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+
+    <BottomNavigationBar />
+    </ThemeProvider>
     </>
   )
 }
