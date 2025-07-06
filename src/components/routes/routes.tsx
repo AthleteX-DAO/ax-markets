b0b40f2eb2cc4cf7dd2850798b5907cb53ff8ebf
Kevin Kamto <kamtokevin@gmail.com>
2025-07-06 12:25:53 -0400

    refactor: temporarily closed routing

--- src/components/routes/routes.tsx
+++ src/components/routes/routes.tsx
@@ -1,12 +1,12 @@
-import { Routes, Route } from 'react-router-dom';
-import TopNavigationBar from '../TopNavigationBar/TopNavigationBar';
+// import { Routes, Route } from 'react-router-dom';
+// import TopNavigationBar from '../TopNavigationBar/TopNavigationBar';
 
 
-const RoutesComponent = () => {
-  return (
-    <Routes>
-      <Route path="/" element={<div>Home Page</div>} />
-      <Route path="*" element={<div>404 Not Found</div>} />
-    </Routes>
-  );
-}
+// const RoutesComponent = () => {
+//   return (
+//     <Routes>
+//       <Route path="/" element={<div>Home Page</div>} />
+//       <Route path="*" element={<div>404 Not Found</div>} />
+//     </Routes>
+//   );
+// }
